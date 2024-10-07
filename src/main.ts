681ea6dba540dbaff37d49c626452e991457e7db
Khaled Ezzughayyar <kz@khaledez.net>
2024-10-07 16:36:22 +0000

    update name to technologies

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <main>
     <header class="container">
       <hgroup>
-        <h1>Banan Systems</h1>
+        <h1>Banan Technologies</h1>
         <p>Coming soon ...</p>
       </hgroup>
     </header>
